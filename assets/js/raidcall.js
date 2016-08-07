9bba48b2b6cf0e8c6d3593563678fa2e7506e109
REH.TW《旋風之音》 <team@reh.tw>
2016-08-07 14:34:48 +0800

    Starts Raidcall Added OS Analyzing

--- assets/js/raidcall.js
+++ assets/js/raidcall.js
@@ -1,7 +1,15 @@
 ﻿/**
- * 進入語音群
+ * 判斷作業系統，進入語音群
  * @param {Number} sid 群ID
  */
 function goChannel(sid) {
-    location.href = 'rctw://enterServer/?sid=' + sid;
+	windows = (navigator.userAgent.indexOf("Windows",0) != -1)?1:0;
+	android = (navigator.userAgent.indexOf("Android",0) != -1)?1:0;
+	if (windows) {
+		location.href = 'rctw://enterServer/?sid=' + sid;
+	} else if (android) {
+		alert("Android 版RC語音無法直接開啟進入語音群，請手動搜尋，群ID：" + sid);
+	} else {
+		alert("您的作業系統無法使用RC語音，請使用支援的作業系統 (Windows 或 Android)。");
+	}
 }
